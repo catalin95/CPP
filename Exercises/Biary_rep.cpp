6f5c856f7e5069e3a1709d8a6b565c2fcf45af6a
Catalin Mucica <catalinmucica@gmail.com>
2020-12-13 23:12:43 +0200

    Add files via upload

---
+++ Exercises/Biary_rep.cpp
@@ -0,0 +1,29 @@
+// Simple C++ program to get the binary of a number
+
+#include <iostream>
+
+
+int binary(int x)
+{
+    if (x == 0)
+        return 0;
+    
+    std::string output, output_string;
+
+    while (x != 0)
+    {
+        output += std::to_string(x % 2);
+        x /= 2;
+    }
+
+    for (int i = output.size() - 1; i != -1; i--)
+        output_string += output[i];
+
+    return std::stoi(output_string);
+}
+
+int main()
+{
+    return 0;
+}
+

bbb62312be263265be0e3c23da08efd45881f175
Catalin Mucica <catalinmucica@gmail.com>
2020-12-25 12:20:37 +0200

    Add files via upload

---
+++ Exercises/work2.cpp
@@ -0,0 +1,32 @@
+// Simple C++ program, for any given number and given power will output a new number composed of the given number digits raised to the given power
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+std::string power(int x, int y)
+{
+   int output = 1;
+   int base = x;
+
+   for (int i = 0; i != y; i++)
+        output *= base;
+
+    return std::to_string(output);
+}
+
+std::string calculate(int x, int y)
+{
+    std::string converted = std::to_string(x);
+    char a;
+    std::string output;
+
+    for (int i = 0; i != converted.size(); i++)
+    {
+        a = converted[i];
+        std::string b(1, a);
+        output += power(std::stoi(b), y);
+    }
+    return output;
+}
+

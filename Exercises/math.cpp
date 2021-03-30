d4fc4c76cfb09e9266b9e4d3cf1aaa341d6001eb
Catalin Mucica <catalinmucica@gmail.com>
2021-03-30 22:08:47 +0300

    Add files via upload

---
+++ Exercises/math.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+class Math
+{
+    public:
+
+        int logarithm(int base, int number)
+        {
+            if (number < 0)
+            {
+                std::cout << "Please provide only positive numbers!\n";
+                return 0;
+            }
+
+            int initial = 1, counter = 0;
+
+            while (initial != number)
+            {
+                initial *= base;
+                if (initial > number)
+                {
+                    std::cout << "Not implemented behaviour, please give a valid base and number\n";
+                    return 0;
+                }
+                else
+                    counter++;
+            }
+
+            return counter;
+        }
+
+        int exponent(int number, int power)
+        {
+            int base = number;
+
+            if (power == 0)
+                return 1;
+            else
+            {
+                for (int i = 0; i != (power - 1); i++)
+                    number *= base;
+            }
+
+            return number;
+        }
+};
